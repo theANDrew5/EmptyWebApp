e23a64a03168e6d3a01cb3e9ca13c686249c0452
andrew <andrey.nik.999@gmail.com>
2021-02-08 18:30:08 +0700

    Exchange page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -9,18 +9,18 @@ namespace EmptyWebApp.Pages
 {
     public class IndexModel : PageModel
     {
-        public string Name { get; set; }
-        public int? Age { get; set; }// знак вопроса говорит о том, что значение может быть null
-        public bool IsCorrect { get; set; } = true;
-        public void OnGet(string name_, int? age_)
-        {
-            if (age_.HasValue!=true || age_<1 || age_>110 || string.IsNullOrEmpty(name_))
-            {
-                IsCorrect = false;
-                return;
-            }
-            Age = age_;
-            Name = name_;
-        }
+    //    public string Name { get; set; }
+    //    public int? Age { get; set; }// знак вопроса говорит о том, что значение может быть null
+    //    public bool IsCorrect { get; set; } = true;
+    //    public void OnGet(string name_, int? age_)
+    //    {
+    //        if (age_.HasValue!=true || age_<1 || age_>110 || string.IsNullOrEmpty(name_))
+    //        {
+    //            IsCorrect = false;
+    //            return;
+    //        }
+    //        Age = age_;
+    //        Name = name_;
+    //    }
     }
 }
